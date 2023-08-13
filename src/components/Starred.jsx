7c281ce8ddb72eb8e1907acded123221f45f084b
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2023-08-13 15:01:41 +0530

    Added nothing avaialble msgs

--- src/components/Starred.jsx
+++ src/components/Starred.jsx
@@ -4,13 +4,17 @@ import MovieCard from "./MovieCard";
 
 const Starred = () => {
     const { appData: { moviesData } } = useContext(AppContext)
-
+    const starredMovies = moviesData.filter(ele => ele.isStarred)
     return (
         <div className="mx-16 mt-24">
             <h3 className="text-2xl font-extrabold text-gray-900">Starred Movies</h3>
             <div className="grid grid-cols-4 gap-8">
                 {
-                    moviesData.filter(ele=>ele.isStarred).map(card=><MovieCard key={card.id} card={card} />)
+                    starredMovies && starredMovies.length > 0 && starredMovies.map(card => <MovieCard key={card.id} card={card} />)
+                }
+                {
+                    starredMovies.length === 0 && <h3 className="text-2xl font-extrabold text-gray-900">Nothing is Starred.</h3>
+
                 }
             </div>
         </div>
--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -5,12 +5,16 @@ import MovieCard from "./MovieCard";
 const Watchlist = () => {
     const { appData: { moviesData } } = useContext(AppContext)
 
+    const watchlistedData = moviesData.filter(ele => ele.watchlisted)
     return (
         <div className="mx-16 mt-24">
             <h3 className="text-2xl font-extrabold text-gray-900">Starred Movies</h3>
             <div className="grid grid-cols-4 gap-8">
                 {
-                    moviesData.filter(ele=>ele.watchlisted).map(card=><MovieCard key={card.id} card={card} />)
+                    watchlistedData && watchlistedData.length > 0 && watchlistedData.map(card => <MovieCard key={card.id} card={card} />)
+                }
+                {
+                    watchlistedData.length === 0 && <h3 className="text-2xl font-extrabold text-gray-900">Please add movies to your watchlist.</h3>
                 }
             </div>
         </div>
