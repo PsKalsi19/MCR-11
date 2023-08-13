0cf0d083c7ff953d5052aa63b7234a2ef1f92b5c
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2023-08-13 15:03:03 +0530

    Updated title

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -8,7 +8,7 @@ const Watchlist = () => {
     const watchlistedData = moviesData.filter(ele => ele.watchlisted)
     return (
         <div className="mx-16 mt-24">
-            <h3 className="text-2xl font-extrabold text-gray-900">Starred Movies</h3>
+            <h3 className="text-2xl font-extrabold text-gray-900">Watchlisted Movies</h3>
             <div className="grid grid-cols-4 gap-8">
                 {
                     watchlistedData && watchlistedData.length > 0 && watchlistedData.map(card => <MovieCard key={card.id} card={card} />)
